47317af805aa18ebd72eb5dda582c8dd622893ea
igor petrov <igor_petrov@epam.com>
2017-09-28 17:32:29 +0400

    config-server for employees-api solution

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,8 +2,10 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.feign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class EmployeesApplication {
     static void main(String[] args) {
         SpringApplication.run(EmployeesApplication, args)
