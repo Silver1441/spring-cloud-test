4fd879c61a20ec8c61e3f583980363d372b5e94c
Ivan <Kishkan91@gmail.com>
2019-11-25 14:59:33 +0400

    gateway api is working

--- infra/api-gateway/src/main/java/com/epam/hw/netflix/ApiGatewayApplication.java
+++ infra/api-gateway/src/main/java/com/epam/hw/netflix/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.epam.hw.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ApiGatewayApplication {
 
